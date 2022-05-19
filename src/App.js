7b04991ef2ebe394160d8488a6b5c09e644ff900
saidabbasun <saidabboscoder0000@gmail.com>
2022-05-19 19:22:28 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './Globl.css'
+import TicTacToe from './TicTacToe/TicTacToe';
+
+function App() {
+	return (
+		<div className='App'>
+			<TicTacToe />
+		</div>
+	);
+}
+
+export default App;
